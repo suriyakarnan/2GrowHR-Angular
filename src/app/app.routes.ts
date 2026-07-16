b14b22dfeff568dcad11136585300eadf1be373e
Suriyakarnan-san <suriyakarnan@saneforce.io>
2026-07-16 09:58:19 +0530

    Changes Completed

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -18,7 +18,7 @@ export const routes: Routes = [
     children: [
       {
         path: 'dashboard',
-        loadChildren: () => import('./features/admin/dashboard/dashboard.component').then(m => m.DashboardComponent)
+        loadComponent: () => import('./features/admin/dashboard/dashboard.component').then(m => m.DashboardComponent)
       },
       {
         path: 'employees',
@@ -85,8 +85,7 @@ export const routes: Routes = [
       },
       {
         path: 'wall-activity',
-        loadComponent: () => import('./features/employee/wall-activity/wall-activity.component').then(m =>m.WallActivityComponent)
-
+        loadComponent: () => import('./features/employee/wall-activity/wall-activity.component').then(m => m.WallActivityComponent)
       },
       { path: '', redirectTo: 'dashboard', pathMatch: 'full' }
     ]
